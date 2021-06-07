beda5742877aa6fcd7904fe69b0d8fd76ff77dbd
madyar997 <madiar.997@gmail.com>
2021-06-07 14:28:57 +0600

    first commit

---
+++ src/main/java/SpringXMLConfigurationMain.java
@@ -0,0 +1,13 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringXMLConfigurationMain {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+//        Country countryBean = (Country) ac.getBean("country");
+//        String name = countryBean.getName();
+//        System.out.println("Country name: " + name);
+//        long population = countryBean.getPopulation();
+//        System.out.println("Country population: " + population);
+//        ac.close();
+    }
+}
